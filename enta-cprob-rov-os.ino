c192db3a93983bbef9c3de3071e146835aa5a227
pmleite <paulo.miguel.r.leite@gmail.com>
2023-11-05 18:13:42 -0100

    proof of concept

--- enta-cprob-rov-os.ino
+++ enta-cprob-rov-os.ino
@@ -1,11 +1,13 @@
 #define BOARD_FUNCTION SENSE  //SENSE or ACTUATE
 
 
-void setup(){
-    Serial.begin(9600);
+void setup() {
+  Serial.begin(9600);
 }
-
-void loop(){
-    Serial.println("Hello World");
-    delay(1000);
+void loop() {
+  if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+    Serial.print("You sent me: ");
+    Serial.println(data);
+  }
 }
--- rovOS.py
+++ rovOS.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-SENSBOARD_PORT    ='/dev/ttyACM0'
-ACTUATEBOARD_PORT ='/dev/ttyACM1'
-
 import serial
-
+import time
 if __name__ == '__main__':
-    ser = serial.Serial(SENSBOARD_PORT, 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
     while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            print(line)
+        ser.write(b"Hello from Raspberry Pi!\n")
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        time.sleep(1)
