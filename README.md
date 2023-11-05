69f166154910d1c505cba593b7fd3679f22f89c6
pmleite <paulo.miguel.r.leite@gmail.com>
2023-11-05 20:03:59 -0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# enta-cpro-rov-os
+# enta-cpro-rov-os #
+
 Firmware do ROV
+
+# Configuring and test RaspberyPy CAM #
+
+run:
+
+```
+sudo rasp-config
+```
+
+Go to interface options and activate camera interface
+
+To teste life video run:
+
+```
+raspivid -t 0
+```
+
+Start straming type
+```
+raspivid -o - -t 0 -hf -w 800 -h 400 -fps 24 |cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8160}' :demux=h264
+```
+
+# dependencias
+pip3 install pyshine==0.0.9
--- rovOS.py
+++ rovOS.py
@@ -1,11 +1,49 @@
 #!/usr/bin/env python3
 import serial
 import time
+import pyshine as ps 
+import cv2
 
-if __name__ == '__main__':
-    sensboard    = serial.Serial('/dev/ttyACM0', 19200, timeout=1)
-    actuateboard = serial.Serial('/dev/ttyACM1', 19200, timeout=1)
+HTML="""
+html><head><title>ROV</title>
+</head>
+<body>
+<h1>ROV</h1>
+<img src="stream.mjpg" width="640" height="480" autoplay playsinline/>
+</body>
+</html>
+"""
+
+def main():
     
+    StreamProps = ps.StreamProps
+    StreamProps.set_Page(StreamProps,HTML)
+    address = ('192.168.1.222',9000)
+    try:
+        StreamProps.set_Mode(StreamProps,'cv2')
+        capture = cv2.VideoCapture('myCampture.mp4')
+        capture.set(cv2.CAP_PROP_FRAME_WIDTH,320)
+        capture.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
+        capture.set(cv2.CAP_PROP_FPS,30)
+        capture.set(cv2.CAP_PROP_BUFFERSIZE,4)
+        StreamProps.set_Capture(StreamProps,capture)
+        StreamProps.set_Quality(StreamProps,90)
+        server = ps.Streamer(address,StreamProps)
+        print("Server started at "+str(address))
+        server.serve_forever()
+    except KeyboardInterrupt:
+        capture.release()
+        server.socket.close()
+        
+        
+    try:
+        sensboard = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+        actuateboard = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
+    except:
+        print("One or more arduino boards are not connected.")
+        print("Please check you connections and try again.")
+        exit()
+        
     sensboard.reset_input_buffer()
     actuateboard.reset_input_buffer()
     
@@ -17,3 +55,11 @@ if __name__ == '__main__':
         line = sensboard.readline().decode('utf-8').rstrip()
         print(line)
         time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
+    
+
+
+    
