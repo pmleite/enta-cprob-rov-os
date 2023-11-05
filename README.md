698c45016c56ffdbf0480b7b46e18d8fcee16880
pmleite <paulo.miguel.r.leite@gmail.com>
2023-11-05 21:22:33 -0100

    camera testes

--- README.md
+++ README.md
@@ -24,7 +24,29 @@ raspivid -o - -t 0 -hf -w 800 -h 400 -fps 24 |cvlc -vvv stream:///dev/stdin --so
 ```
 
 # dependencias
+sudo apt install autoconf automake build-essential pkgconf libtool git libzip-dev libjpeg-dev gettext libmicrohttpd-dev libavformat-dev libavcodec-dev libavutil-dev libswscale-dev libavdevice-dev default-libmysqlclient-dev libpq-dev libsqlite3-dev libwebp-dev
+
+
+sudo wget https://github.com/Motion-Project/motion/releases/download/release-4.5.1/$(lsb_release -cs)_motion_4.5.1-1_$(dpkg --print-architecture
+).deb
+
+sudo dpkg -i $(lsb_release -cs)_motion_4.5.1-1_$(dpkg --print-architecture).deb
+
+
+
+
+
+
+
+
+
+
+
 pip3 install pyshine==0.0.9
+
+
+
+
 
 Local PC
 pip3 install opencv-python
--- videoStream.py
+++ videoStream.py
@@ -1,46 +1,57 @@
 #!/usr/bin/env python3
-import serial
-import time
-import pyshine as ps 
-import cv2
 
-IP_ADDRESS      = '192.168.1.222'
-UDP_STREAM_PORT = 9000
+from picamera import PiCamera
+from time import sleep
 
-HTML="""
-html><head><title>ROV</title>
-</head>
-<body>
-<h1>ROV</h1>
-<img src="stream.mjpg" width="640" height="480" autoplay playsinline/>
-</body>
-</html>
-"""
+camera = PiCamera()
 
-def main():
+camera.start_preview()
+sleep(10)
+camera.stop_preview()
+
+
+# import serial
+# import time
+# import pyshine as ps 
+# import cv2
+
+# IP_ADDRESS      = '192.168.1.222'
+# UDP_STREAM_PORT = 9000
+
+# HTML="""
+# html><head><title>ROV</title>
+# </head>
+# <body>
+# <h1>ROV</h1>
+# <img src="stream.mjpg" width="640" height="480" autoplay playsinline/>
+# </body>
+# </html>
+# """
+
+# def main():
     
-    StreamProps = ps.StreamProps
-    StreamProps.set_Page(StreamProps,HTML)
-    address = (IP_ADDRESS,UDP_STREAM_PORT)
-    try:
-        StreamProps.set_Mode(StreamProps,'cv2')
-        capture = cv2.VideoCapture('myCampture.mp4')
-        capture.set(cv2.CAP_PROP_FRAME_WIDTH,320)
-        capture.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
-        capture.set(cv2.CAP_PROP_FPS,30)
-        capture.set(cv2.CAP_PROP_BUFFERSIZE,4)
-        StreamProps.set_Capture(StreamProps,capture)
-        StreamProps.set_Quality(StreamProps,90)
-        server = ps.Streamer(address,StreamProps)
-        print("Server started at "+str(address))
-        server.serve_forever()
-    except KeyboardInterrupt:
-        capture.release()
-        server.socket.close()
+#     StreamProps = ps.StreamProps
+#     StreamProps.set_Page(StreamProps,HTML)
+#     address = (IP_ADDRESS,UDP_STREAM_PORT)
+#     try:
+#         StreamProps.set_Mode(StreamProps,'cv2')
+#         capture = cv2.VideoCapture('myCampture.mp4')
+#         capture.set(cv2.CAP_PROP_FRAME_WIDTH,320)
+#         capture.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
+#         capture.set(cv2.CAP_PROP_FPS,30)
+#         capture.set(cv2.CAP_PROP_BUFFERSIZE,4)
+#         StreamProps.set_Capture(StreamProps,capture)
+#         StreamProps.set_Quality(StreamProps,90)
+#         server = ps.Streamer(address,StreamProps)
+#         print("Server started at "+str(address))
+#         server.serve_forever()
+#     except KeyboardInterrupt:
+#         capture.release()
+#         server.socket.close()
         
     
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
     
 
 
