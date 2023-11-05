14c3a12d7c779c681bf6c0af102841b6284ae6f4
pmleite <paulo.miguel.r.leite@gmail.com>
2023-11-05 21:12:44 -0100

    update

--- updateAndRun.sh
+++ updateAndRun.sh
@@ -6,6 +6,7 @@ git pull
 
 echo running rovOS
 lxterminal --title="ROV_OS" -e python3 /home/pi/enta-cprob-rov-os/rovOS.py
+echo done
 
 echo runnig videoStreaming
 lxterminal --title="ROV_OS" -e pyhton3 /home/pi/enta-cprob-rov-os/videoStream.py 
