9ba9e1e7c7ec522050586c9cf21239f5a0c77080
pmleite <paulo.miguel.r.leite@gmail.com>
2024-05-29 11:31:11 +0000

    teste

--- motor_control.py
+++ motor_control.py
@@ -12,6 +12,14 @@ MOTOR_B_PIN1 = 19
 MOTOR_B_PIN2 = 26
 MOTOR_B_ENABLE = 13
 
+MOTOR_C_PIN1 = 29
+MOTOR_C_PIN2 = 27
+MOTOR_C_ENABLE = 31
+
+MOTOR_D_PIN1 = 7
+MOTOR_D_PIN2 = 8
+MOTOR_D_ENABLE = 1
+
 # Configuração do GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(MOTOR_A_PIN1, GPIO.OUT)
@@ -20,21 +28,35 @@ GPIO.setup(MOTOR_A_ENABLE, GPIO.OUT)
 GPIO.setup(MOTOR_B_PIN1, GPIO.OUT)
 GPIO.setup(MOTOR_B_PIN2, GPIO.OUT)
 GPIO.setup(MOTOR_B_ENABLE, GPIO.OUT)
+GPIO.setup(MOTOR_C_PIN1, GPIO.OUT)
+GPIO.setup(MOTOR_C_PIN2, GPIO.OUT)
+GPIO.setup(MOTOR_C_ENABLE, GPIO.OUT)
+GPIO.setup(MOTOR_D_PIN1, GPIO.OUT)
+GPIO.setup(MOTOR_D_PIN2, GPIO.OUT)
+GPIO.setup(MOTOR_D_ENABLE, GPIO.OUT)
+
 
 # Configuração PWM
-pwm_a = GPIO.PWM(MOTOR_A_ENABLE, 100)
-pwm_b = GPIO.PWM(MOTOR_B_ENABLE, 100)
+pwm_a = GPIO.PWM(MOTOR_A_ENABLE, 1000)
+pwm_b = GPIO.PWM(MOTOR_B_ENABLE, 1000)
+pwm_c = GPIO.PWM(MOTOR_C_ENABLE, 1000)
+pwm_d = GPIO.PWM(MOTOR_D_ENABLE, 1000)
 pwm_a.start(0)
 pwm_b.start(0)
+pwm_c.start(0)
+pwm_d.start(0)
 
 def set_motor(motor_pins, speed):
+   
     pin1, pin2, pwm = motor_pins
+   
     if speed >= 0:
         GPIO.output(pin1, GPIO.HIGH)
         GPIO.output(pin2, GPIO.LOW)
     else:
         GPIO.output(pin1, GPIO.LOW)
         GPIO.output(pin2, GPIO.HIGH)
+    
     pwm.ChangeDutyCycle(abs(speed))
 
 @app.route('/')
@@ -48,7 +70,9 @@ def control():
 
     set_motor((MOTOR_A_PIN1, MOTOR_A_PIN2, pwm_a), motor_a_speed)
     set_motor((MOTOR_B_PIN1, MOTOR_B_PIN2, pwm_b), motor_b_speed)
-
+    set_motor((MOTOR_C_PIN1, MOTOR_C_PIN2, pwm_c), motor_a_speed)
+    set_motor((MOTOR_D_PIN1, MOTOR_D_PIN2, pwm_d), motor_b_speed)
+    
     return 'OK'
 
 if __name__ == "__main__":
@@ -59,4 +83,6 @@ if __name__ == "__main__":
     finally:
         pwm_a.stop()
         pwm_b.stop()
+        pwm_c.stop()
+        pwm_d.stop()
         GPIO.cleanup()
