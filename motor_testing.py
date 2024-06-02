a2b3fddcf0486d3b006c685c0ea6846b56ccd252
pmleite <paulo.miguel.r.leite@gmail.com>
2024-06-02 12:14:12 +0000

    teste program

--- motor_testing.py
+++ motor_testing.py
@@ -3,48 +3,48 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)  
 
 # Configuração dos pinos
-MOTOR_1_PIN1   = 17
-MOTOR_1_PIN2   = 22
-MOTOR_1_PWM    = 27
+MOTOR_FL_PIN1   = 17
+MOTOR_FL_PIN2   = 22
+MOTOR_FL_PWM    = 27
 
-# MOTOR_2_PIN1   = 23
-# MOTOR_2_PIN2   = 24
-# MOTOR_2_PWM    = 25
+MOTOR_FR_PIN1   = 23
+MOTOR_FR_PIN2   = 24
+MOTOR_FR_PWM    = 25
 
-# MOTOR_3_PIN1   = 21
-# MOTOR_3_PIN2   = 20
-# MOTOR_3_PWM    = 16
+MOTOR_BL_PIN1   = 21
+MOTOR_BL_PIN2   = 20
+MOTOR_BL_PWM    = 16
 
-# MOTOR_4_PIN1   = 13
-# MOTOR_4_PIN2   = 19
-# MOTOR_4_PWM    = 26
+MOTOR_BR_PIN1   = 13
+MOTOR_BR_PIN2   = 19
+MOTOR_BR_PWM    = 26
 
 # GPIO Configuration
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(MOTOR_1_PIN1, GPIO.OUT)
-GPIO.setup(MOTOR_1_PIN2, GPIO.OUT)
-GPIO.setup(MOTOR_1_PWM, GPIO.OUT)
-# GPIO.setup(MOTOR_2_PIN1, GPIO.OUT)
-# GPIO.setup(MOTOR_2_PIN2, GPIO.OUT)
-# GPIO.setup(MOTOR_2_PWM, GPIO.OUT)
-# GPIO.setup(MOTOR_3_PIN1, GPIO.OUT)
-# GPIO.setup(MOTOR_3_PIN2, GPIO.OUT)
-# GPIO.setup(MOTOR_3_PWM, GPIO.OUT)
-# GPIO.setup(MOTOR_4_PIN1, GPIO.OUT)
-# GPIO.setup(MOTOR_4_PIN2, GPIO.OUT)
-# GPIO.setup(MOTOR_4_PWM, GPIO.OUT)
+GPIO.setup(MOTOR_FL_PIN1, GPIO.OUT)
+GPIO.setup(MOTOR_FL_PIN2, GPIO.OUT)
+GPIO.setup(MOTOR_FL_PWM,  GPIO.OUT)
+GPIO.setup(MOTOR_FR_PIN1, GPIO.OUT)
+GPIO.setup(MOTOR_FR_PIN2, GPIO.OUT)
+GPIO.setup(MOTOR_FR__PWM, GPIO.OUT)
+GPIO.setup(MOTOR_BL_PIN1, GPIO.OUT)
+GPIO.setup(MOTOR_BL_PIN2, GPIO.OUT)
+GPIO.setup(MOTOR_BL_PWM,  GPIO.OUT)
+GPIO.setup(MOTOR_BR_PIN1, GPIO.OUT)
+GPIO.setup(MOTOR_BR_PIN2, GPIO.OUT)
+GPIO.setup(MOTOR_BR_PWM,  GPIO.OUT)
 print("GPIO Configured")
 
 # PWM Initialization
-pwm_1 = GPIO.PWM(MOTOR_1_PWM, 100)
-# pwm_2 = GPIO.PWM(MOTOR_2_PWM, 100)
-# pwm_3 = GPIO.PWM(MOTOR_3_PWM, 100)
-# pwm_4 = GPIO.PWM(MOTOR_4_PWM, 100)
+pwm_1 = GPIO.PWM(MOTOR_FL_PWM, 100)
+pwm_2 = GPIO.PWM(MOTOR_FR_PWM, 100)
+pwm_3 = GPIO.PWM(MOTOR_BL_PWM, 100)
+pwm_4 = GPIO.PWM(MOTOR_BR_PWM, 100)
 print("PWM Initialized")
 pwm_1.start(0)
-# pwm_2.start(0)
-# pwm_3.start(0)
-# pwm_4.start(0)
+pwm_2.start(0)
+pwm_3.start(0)
+pwm_4.start(0)
 print("PWM Started")
 
 def set_motor(motor_pins, speed):
@@ -64,10 +64,10 @@ def teste():
     # Test
     while True: 
         print("Teste")
-        set_motor((MOTOR_1_PIN1, MOTOR_1_PIN2, pwm_1), 100)
-        # set_motor((MOTOR_2_PIN1, MOTOR_2_PIN2, pwm_2), 100)
-        # set_motor((MOTOR_3_PIN1, MOTOR_3_PIN2, pwm_3), 100)
-        # set_motor((MOTOR_4_PIN1, MOTOR_4_PIN2, pwm_4), 100)
+        set_motor((MOTOR_FL_PIN1, MOTOR_FL_PIN2, pwm_1), 100)
+        set_motor((MOTOR_FR_PIN1, MOTOR_FR_PIN2, pwm_2), 100)
+        set_motor((MOTOR_BL_PIN1, MOTOR_BL_PIN2, pwm_3), 100)
+        set_motor((MOTOR_BR_PIN1, MOTOR_BR_PIN2, pwm_4), 100)
 
 
 if __name__ == "__main__":
@@ -78,9 +78,9 @@ if __name__ == "__main__":
     finally:
         print("Cleaning up")
         pwm_1.stop()
-        # pwm_2.stop()
-        # pwm_3.stop()
-        # pwm_4.stop()
+        pwm_2.stop()
+        pwm_3.stop()
+        pwm_4.stop()
         GPIO.cleanup()
         print("Program Finished")
 
