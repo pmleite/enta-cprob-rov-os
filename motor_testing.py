4543b781b4860e15b4e9e12a1cfd0f1cd7d0c566
pmleite <paulo.miguel.r.leite@gmail.com>
2024-06-02 15:06:11 +0000

    teste program

--- motor_testing.py
+++ motor_testing.py
@@ -6,6 +6,10 @@ Motor1A = 17
 Motor1B = 27
 Motor2A = 22
 Motor2B = 23
+Motor3A = 24
+Motor3B = 25
+Motor4A = 8
+Motor4B = 7
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)    # Use BCM pin numbering
@@ -13,18 +17,30 @@ GPIO.setup(Motor1A, GPIO.OUT)
 GPIO.setup(Motor1B, GPIO.OUT)
 GPIO.setup(Motor2A, GPIO.OUT)
 GPIO.setup(Motor2B, GPIO.OUT)
+GPIO.setup(Motor3A, GPIO.OUT)
+GPIO.setup(Motor3B, GPIO.OUT)
+GPIO.setup(Motor4A, GPIO.OUT)
+GPIO.setup(Motor4B, GPIO.OUT)
 
 # Set up PWM on the GPIO pins
 pwm1A = GPIO.PWM(Motor1A, 100)  # PWM with 100Hz frequency on Motor1A
 pwm1B = GPIO.PWM(Motor1B, 100)  # PWM with 100Hz frequency on Motor1B
 pwm2A = GPIO.PWM(Motor2A, 100)  # PWM with 100Hz frequency on Motor2A
 pwm2B = GPIO.PWM(Motor2B, 100)  # PWM with 100Hz frequency on Motor2B
+pwm3A = GPIO.PWM(Motor3A, 100)  # PWM with 100Hz frequency on Motor1A
+pwm3B = GPIO.PWM(Motor3B, 100)  # PWM with 100Hz frequency on Motor1B
+pwm4A = GPIO.PWM(Motor4A, 100)  # PWM with 100Hz frequency on Motor2A
+pwm4B = GPIO.PWM(Motor4B, 100)  # PWM with 100Hz frequency on Motor2B
 
 # Start PWM with a duty cycle of 0 (motors off)
 pwm1A.start(0)
 pwm1B.start(0)
 pwm2A.start(0)
 pwm2B.start(0)
+pwm3A.start(0)
+pwm3B.start(0)
+pwm4A.start(0)
+pwm4B.start(0)
 
 def set_motor1_speed(speed):
     if speed > 0:
@@ -72,4 +88,8 @@ pwm1A.stop()
 pwm1B.stop()
 pwm2A.stop()
 pwm2B.stop()
+pwm3A.stop()
+pwm3B.stop()
+pwm4A.stop()
+pwm4B.stop()
 GPIO.cleanup()
