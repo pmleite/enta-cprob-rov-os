3e8da02f939f67e4217abef857336034bb5a4b3b
pmleite <paulo.miguel.r.leite@gmail.com>
2024-06-02 11:29:03 +0000

    teste program

--- motor_testing.py
+++ motor_testing.py
@@ -34,10 +34,10 @@ GPIO.setup(MOTOR_1_PWM, GPIO.OUT)
 print("GPIO Configured")
 
 # PWM Initialization
-pwm_1 = GPIO.PWM(MOTOR_1_PWM, 1000)
-# pwm_2 = GPIO.PWM(MOTOR_2_PWM, 1000)
-# pwm_3 = GPIO.PWM(MOTOR_3_PWM, 1000)
-# pwm_4 = GPIO.PWM(MOTOR_4_PWM, 1000)
+pwm_1 = GPIO.PWM(MOTOR_1_PWM, 100)
+# pwm_2 = GPIO.PWM(MOTOR_2_PWM, 100)
+# pwm_3 = GPIO.PWM(MOTOR_3_PWM, 100)
+# pwm_4 = GPIO.PWM(MOTOR_4_PWM, 100)
 print("PWM Initialized")
 pwm_1.start(0)
 # pwm_2.start(0)
@@ -62,10 +62,10 @@ def teste():
     # Test
     while True: 
         print("Teste")
-        set_motor((MOTOR_1_PIN1, MOTOR_1_PIN2, pwm_1), 1000)
-        # set_motor((MOTOR_2_PIN1, MOTOR_2_PIN2, pwm_2), 1000)
-        # set_motor((MOTOR_3_PIN1, MOTOR_3_PIN2, pwm_3), 1000)
-        # set_motor((MOTOR_4_PIN1, MOTOR_4_PIN2, pwm_4), 1000)
+        set_motor((MOTOR_1_PIN1, MOTOR_1_PIN2, pwm_1), 100)
+        # set_motor((MOTOR_2_PIN1, MOTOR_2_PIN2, pwm_2), 100)
+        # set_motor((MOTOR_3_PIN1, MOTOR_3_PIN2, pwm_3), 100)
+        # set_motor((MOTOR_4_PIN1, MOTOR_4_PIN2, pwm_4), 100)
 
 
 if __name__ == "__main__":
